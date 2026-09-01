f8a0277842674d0f92dda239d895c8dbf808de4e
Aksa <aksageorge2703@gmail.com>
2026-09-01 17:28:47 +0530

    fix: variables in service infra

--- service_infra/variable.tf
+++ service_infra/variable.tf
@@ -4,58 +4,58 @@ variable "region" {
   default     = "ap-southeast-1"
 }
 
-variable "vpc_id" {
+# variable "vpc_id" {
   
-}
+# }
 
-variable "listener_https_arn" {
+# variable "listener_https_arn" {
   
-}
+# }
 
-variable "user_repository_url" {
+# variable "user_repository_url" {
   
-}
+# }
 
-variable "order_repository_url" {
+# variable "order_repository_url" {
   
-}
+# }
 
-variable "notification_repository_url" {
+# variable "notification_repository_url" {
   
-}
+# }
 
-variable "POSTGRES_USER" {
+# variable "POSTGRES_USER" {
   
-}
+# }
 
-variable "POSTGRES_HOST" {
+# variable "POSTGRES_HOST" {
   
-}
+# }
 
-variable "master_user_secret_arn" {
+# variable "master_user_secret_arn" {
   
-}
+# }
 
-variable "AWS_S3_BUCKET" {
+# variable "AWS_S3_BUCKET" {
   
-}
+# }
 
-variable "AWS_QUEUE_URL" {
+# variable "AWS_QUEUE_URL" {
   
-}
+# }
 
-variable "private_subnet_id" {
+# variable "private_subnet_id" {
   
-}
+# }
 
-variable "User_Security_Group" {
+# variable "User_Security_Group" {
   
-}
+# }
 
-variable "Notification_Security_Group" {
+# variable "Notification_Security_Group" {
   
-}
+# }
 
-variable "Order_Security_Group" {
+# variable "Order_Security_Group" {
   
-}
+# }
