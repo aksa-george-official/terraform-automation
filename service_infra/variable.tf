16746ba241f4a307a9cdf70dd74f157404547a51
Aksa <aksageorge2703@gmail.com>
2026-09-01 11:42:14 +0530

    Fix : input variables for service infra

--- service_infra/variable.tf
+++ service_infra/variable.tf
@@ -3,3 +3,59 @@ variable "region" {
   type        = string
   default     = "ap-southeast-1"
 }
+
+variable "vpc_id" {
+  
+}
+
+variable "listener_https_arn" {
+  
+}
+
+variable "user_repository_url" {
+  
+}
+
+variable "order_repository_url" {
+  
+}
+
+variable "notification_repository_url" {
+  
+}
+
+variable "POSTGRES_USER" {
+  
+}
+
+variable "POSTGRES_HOST" {
+  
+}
+
+variable "master_user_secret_arn" {
+  
+}
+
+variable "AWS_S3_BUCKET" {
+  
+}
+
+variable "AWS_QUEUE_URL" {
+  
+}
+
+variable "private_subnet_id" {
+  
+}
+
+variable "User_Security_Group" {
+  
+}
+
+variable "Notification_Security_Group" {
+  
+}
+
+variable "Order_Security_Group" {
+  
+}
