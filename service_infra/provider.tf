b7ade71c141ace85062883bcacbc0a65dff336e8
Aksa <aksageorge2703@gmail.com>
2026-09-01 15:05:40 +0530

    fix: data block only

--- service_infra/provider.tf
+++ service_infra/provider.tf
@@ -17,7 +17,7 @@ data "terraform_remote_state" "source_stack" {
   backend = "remote"
   config = {
     hostname = "aksa-george-official.app.spacelift.io"
-    organization = "aksa-george-official.app"
+    organization = "aksa-george-official"
     workspaces = {
       name = "aksa-base-infra"
       }
